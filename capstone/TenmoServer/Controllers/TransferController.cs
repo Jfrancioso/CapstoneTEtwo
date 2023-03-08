40126448bc186c68434a3589ea1cce876cd889f6
John Lindsey <johnlindseyjl14@gmail.com>
2023-03-08 14:35:08 -0500

    Set up a basic TransferController

---
+++ capstone/TenmoServer/Controllers/TransferController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TenmoServer.DAO;
+
+namespace TenmoServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TransferController : ControllerBase
+    {
+        private readonly IAccountDao accountDao;
+
+        public TransferController(IAccountDao _accountDao)
+        {
+            accountDao = _accountDao;
+        }
+
+
+    }
+}
