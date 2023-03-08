651a317c9d2bf3d8b099ce033b7896f2be1267d5
John Lindsey <johnlindseyjl14@gmail.com>
2023-03-08 14:52:15 -0500

    Completed controller method for retrieving a transfer by id

--- capstone/TenmoServer/Controllers/TransferController.cs
+++ capstone/TenmoServer/Controllers/TransferController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TenmoServer.DAO;
+using TenmoServer.Models;
 
 namespace TenmoServer.Controllers
 {
@@ -11,10 +12,18 @@ namespace TenmoServer.Controllers
     public class TransferController : ControllerBase
     {
         private readonly IAccountDao accountDao;
+        private readonly ITransferDao transferDao;
 
-        public TransferController(IAccountDao _accountDao)
+        public TransferController(IAccountDao _accountDao, ITransferDao _transferDao)
         {
             accountDao = _accountDao;
+            transferDao = _transferDao;
+        }
+
+        [HttpGet("transfer/{transferId}")]
+        public Transfer GetTransferDetailsById(int transferId)
+        {
+            return transferDao.TransferDetails(transferId);
         }
 
 
